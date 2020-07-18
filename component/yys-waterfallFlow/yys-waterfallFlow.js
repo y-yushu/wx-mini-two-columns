773bae1ab52e547e5c1a4645d5d2501f510ad9ec
七月十二 <2671255784@qq.com>
2020-07-18 23:15:13 +0800

    新增自定义样式，新增自定义节点，新增触底刷新

--- component/yys-waterfallFlow/yys-waterfallFlow.js
+++ component/yys-waterfallFlow/yys-waterfallFlow.js
@@ -1,9 +1,33 @@
 // component/yys-waterfallFlow/yys-waterfallFlow.js
 Component({
   /**
+   * 在组件定义时的选项中启用多slot支持
+   */
+  options: {
+    multipleSlots: true
+  },
+
+  /**
+   * 引入自定义样式
+   */
+  externalClasses: [
+    // 图片自定义样式
+    'class-image',
+    'class-image-left',
+    'class-image-right',
+    // 标题自定义样式
+    'class-title',
+  ],
+
+  /**
    * 组件的属性列表
    */
   properties: {
+    // 标准数据是否为总集合
+    isNoramalDataOrList:{
+      type: Boolean,
+      value: false
+    },
     // 标准数据
     noramalData:{
       type: Array,
@@ -13,11 +37,12 @@ Component({
       type: String,
       value: '0'
     },
-    // 瀑布上下间距 默认40
-    waterfallGap:{
-      type:Number,
-      value: 40
-    },
+    // 自定义组件高度 单位px
+    // 传入后组件不调用获取自定义组件高度方法，加载速度加快
+    cuSlotHeight1:{
+      type: Number,
+      value: 0
+    }
   },
 
   /**
@@ -29,16 +54,35 @@ Component({
     rightList: [],  // 右瀑布
     leftHight: 0,   // 左瀑布高度
     rightHight: 0,  // 右瀑布高度
+    isInitialize: false,  // 是否初始化
+    waterfallGap: 40, // 标题高度
+    isSlotHeight: false, // 是否获取自定义组件高度
+    cuSlotHeight2: 0,  // 自定义组件高度
   },
 
   /**
-   * 生命周期--组件挂载前
+   * 监听数据
+   */
+  observers:{
+    noramalData(val){
+      if(!this.data.isInitialize){ return };  // 未初始化拦截
+      if(this.properties.isNoramalDataOrList){  // 判断是否仅遍历新增数据
+        var arr = val.removeDuplication(this.data.leftList,this.data.rightList);
+        this.getNoramalData(arr);  // 【获取瀑布数据】
+      }else{
+        this.getNoramalData(val);  // 【获取瀑布数据】
+      }
+    }
+  },
+
+  /**
+   * 生命周期--组件挂载前 yyshu 20200718
    */
   attached(){
     const that = this;
     that.createSelectorQuery().select('.page_left').boundingClientRect(res => {
       that.setData({waterfallWidth: res.width});  // 获取瀑布宽度，并赋值
-      that.getNoramalData(); // 【获取瀑布数据】
+      that.getNoramalData(that.properties.noramalData,true); // 【获取瀑布数据】并初始化监听数据
     }).exec();
   },
 
@@ -48,59 +92,104 @@ Component({
   methods: {
 
     /**
-     * 方法--【获取瀑布数据】
+     * 方法--【获取瀑布数据】 yyshu 20200718
+     * @param {Boolean} type => 是否是初始化 
      */
-    getNoramalData(){
+    getNoramalData(noramalData,type = false){
+      if(type){ this.data.isInitialize = true }; // 获取宽度后初始化
+      console.log('【新增数据】',noramalData);
       var that = this;
-      const noramalData = that.properties.noramalData;
       var waterfallWidth = that.data.waterfallWidth;
       noramalData.forEach((element,i) => {
         wx.getImageInfo({   // 遍历获取图片数据
           src: element.image,
           success(res){
-            element.Height = parseInt(Math.round(res.height * waterfallWidth / res.width));
-            that.setList(element,i);  // 调用【瀑布填充】方法
+            element.Height = parseInt(Math.round(res.height * waterfallWidth / res.width)); // 图片高度计算
+            that.setList(element);  // 调用【瀑布填充】方法
           }
         })
       })
-
     },
 
     /**
-     * 方法--【瀑布填充】
+     * 方法--【瀑布填充】 yyshu 20200718
      * @param {Array} element => 瀑布内容 
-     * @param {num} i => 遍历下标
      */
-    setList(element,i){
-      console.log(i,element)
-      var that = this;
+    setList(element){
       var leftH = this.data.leftHight;    // 左瀑布高度
       var rightH = this.data.rightHight;  // 右瀑布高度
       var leftData = this.data.leftList;  // 左瀑布内容
       var rightData = this.data.rightList; // 右瀑布内容
       // 判断是否有标题
-      var waterfallGap = 0;
-      if(true){
-        waterfallGap = this.properties.waterfallGap;
+      if(!element.title){
+        element.waterfallGap = this.properties.waterfallGap;
+      }else{
+        // 是否传入了自定义组件高度
+        if(this.properties.cuSlotHeight1 !== 0){
+          this.getSlotHeight(this.data.isSlotHeight); // 延时严重，不使用自动获取
+          element.waterfallGap = this.properties.cuSlotHeight2;
+        }else{
+          element.waterfallGap = this.properties.cuSlotHeight1;
+        }
       }
       // 判断瀑布长度，短瀑布增加
       if (leftH == rightH || leftH < rightH) {
-        leftH += element.Height + waterfallGap;
+        leftH += element.Height + element.waterfallGap;
         leftData.push(element);
         this.setData({
           leftList: leftData,
           leftHight: leftH,
         })
       } else {
-        rightH += element.Height + waterfallGap;
+        rightH += element.Height + element.waterfallGap;
         rightData.push(element);
         this.setData({
           rightList: rightData,
           rightHight: rightH,
         })
-      }
-      
-    }
+      };
+    },
 
-  }
+    /**
+     * 方法--【获取自定义组件高度】
+     * 因为延时太严重了，不适用自动获取方法 yyshu 20200717
+     */
+    getSlotHeight(type){
+      if(type){ return };
+      const that = this;
+      that.createSelectorQuery().select('.slot-title').boundingClientRect((res) => {
+        if(!res){ return };
+        that.setData({
+          isSlotHeight: true,
+          cuSlotHeight2: Math.round(res.height),
+        })
+      }).exec();
+    },
+
+    /**
+     * 方法--【点击元素】
+     * @param {List} data => 点击元素 
+     */
+    clickDom(data){
+      this.triggerEvent("clickDom", data.currentTarget.dataset.data);
+    },
+  },
 })
+
+/**
+ * 原型方法--数组去重
+ */
+Array.prototype.removeDuplication = function(arr1,arr2){
+  var arr0 = [],ifHas = false;
+  this.forEach((ret) => {
+    ifHas = false;
+    arr1.forEach((ele1) => {
+      if(ele1.id == ret.id) ifHas = true;
+    })
+    arr2.forEach((ele2) => {
+      if(ele2.id == ret.id) ifHas = true;
+    })
+    if(!ifHas) arr0.push(ret);
+  })
+  return arr0;
+}
--- index/index.js
+++ index/index.js
@@ -2,18 +2,66 @@ const app = getApp()
 
 Page({
   data: {
-    images:[
-      {image:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1133375469,702564064&fm=26&gp=0.jpg'},
-      {image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595048329096&di=3924f6904e9eabe31a020134dbc5ee8c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn16%2F240%2Fw640h400%2F20181119%2F22df-hmivixn3889803.jpg'},
-      {image:'http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190313090409/完美9.png'},
-      {image:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2126590754,116561686&fm=26&gp=0.jpg'},
-      {image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=128144883,1495567763&fm=26&gp=0.jpg'},
-      {image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2223828760,2771029196&fm=26&gp=0.jpg'},
-      {image:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3600861892,4082801463&fm=26&gp=0.jpg'},
+    pageInfo:[],
+    pageData:[],
+    analogData:[
     ],
+    middleGap:'5px'
   },
   onLoad: function () {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
+    // 获取模拟数据
+    var arr1 = [],arr2 = [];
+    moNiData.forEach((ele,i) => {
+      ele.id = i + '-' + new Date().getTime(); // 模拟获取随机ID
+      arr1.push(ele);
+      arr2.push(ele);
+    });
+    this.setData({
+      pageInfo: arr1,
+      pageData: arr2,
+    })
+  },
+
+  onReachBottom(){
+    console.log('小程序触底，触发加载数据')
+    this.addPageInfo();
+  },
+
+  // 获取点击事件
+  test(data){
+    console.log(data)
+  },
+  // 增加数据
+  addPageInfo(){
+    var arr = this.data.pageInfo; // 总数据集合（节点ID使用）
+    var arr2 = [];    // 新增数据集合（避免图片重复加载，导致速度过慢）
+    var data = [
+      {id:'0',image:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1133375469,702564064&fm=26&gp=0.jpg'},
+      {id:'1',image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595048329096&di=3924f6904e9eabe31a020134dbc5ee8c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn16%2F240%2Fw640h400%2F20181119%2F22df-hmivixn3889803.jpg'},
+      {id:'2',image:'http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190313090409/完美9.png'},
+      {id:'3',image:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2126590754,116561686&fm=26&gp=0.jpg'},
+      {id:'4',image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=128144883,1495567763&fm=26&gp=0.jpg'},
+      {id:'5',image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2223828760,2771029196&fm=26&gp=0.jpg'},
+      {id:'6',image:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3600861892,4082801463&fm=26&gp=0.jpg'},
+    ];
+    data.forEach((ele,i) => {
+      ele.id = i + '-' + new Date().getTime(); // 模拟获取随机ID
+      arr.push(ele);
+      arr2.push(ele);
+    });
+    this.setData({
+      pageInfo: arr,
+      pageData: arr2
+    })
   },
 })
+
+var moNiData = [
+  {id:'0',image:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1133375469,702564064&fm=26&gp=0.jpg'},
+  {id:'1',image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595048329096&di=3924f6904e9eabe31a020134dbc5ee8c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn16%2F240%2Fw640h400%2F20181119%2F22df-hmivixn3889803.jpg'},
+  {id:'2',image:'http://dashus.oss-cn-shenzhen.aliyuncs.com/DefaultImage/Game/20190313090409/完美9.png'},
+  {id:'3',image:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2126590754,116561686&fm=26&gp=0.jpg'},
+  {id:'4',image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=128144883,1495567763&fm=26&gp=0.jpg'},
+  {id:'5',image:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2223828760,2771029196&fm=26&gp=0.jpg'},
+  {id:'6',image:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3600861892,4082801463&fm=26&gp=0.jpg'},
+];
